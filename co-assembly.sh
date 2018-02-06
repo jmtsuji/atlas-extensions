faf908877210fc93315ce15a032793caef8faf9a
Jackson Tsuji <jackson.tsuji@uwaterloo.ca>
2018-02-06 17:55:12 -0500

    Improved coassembly info input

--- co-assembly.sh
+++ co-assembly.sh
@@ -16,34 +16,32 @@ script_version="1.0.22r1" # to match ATLAS version it is designed to work with
 # If no input is provided, exit out and provide help
 if [ $# == 0 ]
     then
-    printf "\n$(basename $0): finds and replaces selected text in a file.\n"
+    printf "$(basename $0): ______.\n"
     printf "Version: ${script_version}\n"
     printf "Contact Jackson M. Tsuji (jackson.tsuji@uwaterloo.ca) for bug reports or feature requests.\n\n"
-    printf "Usage: $(basename $0) output_dir database_dir assembly_samples.list mapping_samples.list\n\n"
+    printf "Usage: $(basename $0) output_dir database_dir config_filepath coassembly_guide_filepath threads\n\n"
     printf "Usage details:\n"
-    printf "- output_dir: Path to the base directory where ATLAS files were output.\n"
-    printf "- database_dir: Path to the ATLAS database directory.\n"
-    printf "- assembly_samples.list: Text list of the ATLAS sample names of samples that should be combined for co-assembly.\n"
-    printf "- mapping_samples.list: Text list of the ATLAS sample names of samples that should be read mapped onto the co-assembly before binning.\n\n"
+    printf "1. output_dir: Path to the base directory where ATLAS files were output.\n"
+    printf "2. database_dir: Path to the ATLAS database directory.\n"
+    printf "3. config_filepath: Text list of the ATLAS sample names of samples that should be combined for co-assembly.\n"
+    printf "4. coassembly_guide_filepath: TSV file with three columns: coassembly_name (names of coassembly runs); assembly_samples (comma-separated names of previously run ATLAS samples to coassemble); read_mapping_samples (comma-separated names of previously run ATLAS samples to read map for binning).\n"
+    printf "5. threads: number of threads to run.\n\n"
     exit 1
 fi
 # Using printf: http://stackoverflow.com/a/8467449 (accessed Feb 21, 2017)
 # Test for empty variable: Bioinformatics Data Skills Ch. 12 pg 403-404, and http://www.tldp.org/LDP/Bash-Beginners-Guide/html/sect_09_07.html and http://stackoverflow.com/a/2428006 (both accessed Feb 21, 2017)
 
-# Get date and time
-DATETIME=$(date '+%y%m%d_%H%M')
-
 # Set variables from user input:
 OUTPUT_DIR=$1
 DATABASE_DIR=$2
-ASSEMBLY_SAMPLES=$3
-MAPPING_SAMPLES=$4
+CONFIG_FILEPATH=$3
+COASSEMBLY_GUIDE_FILEPATH=$4
+THREADS=14 # TODO Eventually need to set this to user input
 
 # Constants
 ASSEMBLY_BINARIES="${OUTPUT_DIR}/.snakemake/conda/ccef13c9/bin"
 BINNING_BINARIES="${OUTPUT_DIR}/.snakemake/conda/a6a01fdc/bin"
-CO_ASSEMBLY_NAME="co_assembly_${DATETIME}"
-CO_ASSEMBLY_DIR="${OUTPUT_DIR}/${CO_ASSEMBLY_NAME}"
+
 
 function test_inputs {
 	# Description: tests that provided folders and files exist in the proper configuration
@@ -78,37 +76,145 @@ function test_inputs {
 		echo "ERROR: Cannot find conda binaries for genome binning in ${BINNING_BINARIES}. Have you run ATLAS normally yet? Exiting..."
 		quit 1
 	fi
-	
-	# Make co-assembly directory
-	if [ ! -d ${CO_ASSEMBLY_DIR} ]; then
-		mkdir ${CO_ASSEMBLY_DIR}
-		echo "Results will be stored in ${CO_ASSEMBLY_DIR}"
-		echo ""
-	else
-		echo "ERROR: found pre-existing output co-assembly directory ${CO_ASSEMBLY_DIR}. Please delete before starting. Exiting..."
-		quit 1
-	fi
 		
 }
 
-function get_assembly_samples {
-	# Description: parses the input assembly sample names list into an array and checks they exist
-	# Params: list of assembly sample names (.list)
-	# Return: assembly_names (array of assembly_names)
+function get_coassembly_params {
+	# Description: parses the coassembly guide file to get coassembly names, files to assembly, files to map. Checks they exist.
+	# GLOBAL params: COASSEMBLY_GUIDE_FILEPATH, OUTPUT_DIRECTORY
+	# Local params: none
+	# Return: coassembly_names (array); assembly_samples (array); read_mapping_samples (array)
 	
-	local assembly_list=$1
+	# Check headers of COASSEMBLY_GUIDE
+	local coassembly_headers=($(head -n 1 ${COASSEMBLY_GUIDE_FILEPATH}))
+	if [ ${#coassembly_headers[@]} != 3 ]; then
+		# TODO improve this error message.
+		echo "ERROR: coassembly guide file has incorrect number of columns. Looking for 3 but found ${#coassembly_headers[@]}. Job terminating."
+		exit 1
+	elif [ ${coassembly_headers[0]} != "coassembly_name" -o  ${coassembly_headers[1]} != "assembly_samples" \
+		-o ${coassembly_headers[2]} != "read_mapping_samples" ]; then
+		# TODO improve this error message.
+		echo "ERROR: coassembly guide file has incorrect headers. Job terminating."
+		exit 1
+	fi
 	
-	assembly_names=($(cat ${assembly_list}))
+	# Get params
+	coassembly_names=($(tail -n +2 ${COASSEMBLY_GUIDE_FILEPATH} | cut -d $'\t' -f 1))
+	assembly_samples=($(tail -n +2 ${COASSEMBLY_GUIDE_FILEPATH} | cut -d $'\t' -f 2))
+	read_mapping_samples=($(tail -n +2 ${COASSEMBLY_GUIDE_FILEPATH} | cut -d $'\t' -f 3))
 	
-	for name in ${assembly_names[@]}; do
-		atlas_path="${OUTPUT_DIR}/${name}"
+	# Test params are okay
+	check_coassembly_params
+	
+}
+
+function check_coassembly_params {
+	# Description: checks that each specified sample has been run through ATLAS and that coassembly directories do not already exist (makes them at the same time)
+	# GLOBAL params: OUTPUT_DIRECTORY; coassembly_names (array); assembly_samples (array); read_mapping_samples (array)
+	# Local params: none
+	# Return: none
+
+	check_and_make_coassembly_dirs
+	check_assembly_samples
+	check_read_mapping_samples
+
+}
+
+function check_assembly_samples {
+	# Description: tests if each input assembly sample exists
+	# GLOBAL params: OUTPUT_DIR, assembly_samples (array)
+	# Local params: none
+	# Return: none
+	
+	# Temporarily change the internal fields separator (IFS) to parse comma separators. See Vince Buffalo's "Bioinformatics Data Skills" (1st Ed.) chapter 12, pg 407 and corresponding Github page README at https://github.com/vsbuffalo/bds-files/tree/master/chapter-12-pipelines (accessed Nov 19, 2017)
+	OFS="$IFS"
+	IFS=,
+	
+	# Each coassembly_name has several associated assembly_samples separated by commas. Examine one coassembly_name at a time.
+	for coassembly_sample in ${assembly_samples[@]}; do
 		
-		if [ ! -d ${atlas_path} ]; then
-			echo "ERROR: Cannot find path for required assembly sample ${atlas_path}. Exiting..."
-			quit 1
-		fi
+		# Get names of individual samples provided for that coassembly_name
+		assembly_sample_IDs=($(cat ${coassembly_sample}))
+		
+		# Iteratively check each assembly_sample to see if it exists and exit if not.
+		for sample_ID in ${assembly_sample_IDs[@]}; do
+			atlas_path="${OUTPUT_DIR}/${sample_ID}"
+			qc_filepath="${atlas_path}/sequence_quality_control"
+			
+			if [ ! -d ${atlas_path} ]; then
+				echo "ERROR: ${sample_ID}: Cannot find path for assembly sample. Looking in '${atlas_path}'. Exiting..."
+				exit 1
+			elif [ ! -f ${qc_filepath}/${sample_ID}_QC_*.fastq.gz ]; then
+				echo "ERROR: ${sample_ID}: Cannot find finished QC files following pattern '${sample_ID}_QC_*.fastq.gz' in '${qc_filepath}'. Exiting..."
+				exit 1 
+			fi
+			
+		done
+		
 	done
 	
+	# Fix the IFS
+	IFS="$OFS"
+	
+}
+
+function check_read_mapping_samples {
+	# Description: tests if each input mapping sample exists
+	# GLOBAL params: OUTPUT_DIR, read_mapping_samples (array)
+	# Local params: none
+	# Return: none
+	
+	# Temporarily change the internal fields separator (IFS) to parse comma separators. See Vince Buffalo's "Bioinformatics Data Skills" (1st Ed.) chapter 12, pg 407 and corresponding Github page README at https://github.com/vsbuffalo/bds-files/tree/master/chapter-12-pipelines (accessed Nov 19, 2017)
+	OFS="$IFS"
+	IFS=,
+	
+	# Each coassembly_name has several associated assembly_samples separated by commas. Examine one coassembly_name at a time.
+	for coassembly_sample in ${read_mapping_samples[@]}; do
+		
+		# Get names of individual samples provided for that coassembly_name
+		read_mapping_sample_IDs=($(cat ${coassembly_sample}))
+		
+		# Iteratively check each assembly_sample to see if it exists and exit if not.
+		for sample_ID in ${read_mapping_sample_IDs[@]}; do
+			atlas_path="${OUTPUT_DIR}/${sample_ID}"
+			qc_filepath="${atlas_path}/sequence_quality_control"
+			
+			if [ ! -d ${atlas_path} ]; then
+				echo "ERROR: ${sample_ID}: Cannot find path for read mapping sample. Looking in '${atlas_path}'. Exiting..."
+				exit 1
+			elif [ ! -f ${qc_filepath}/${sample_ID}_QC_se.fastq.gz ]; then
+				# TODO: confirm that se reads will be present both for paired end and R1/R2. This test might not be sufficient right now.
+				echo "ERROR: ${sample_ID}: Cannot find finished QC files '${sample_ID}_QC_se.fastq.gz' in '${qc_filepath}'. Exiting..."
+				exit 1 
+			fi
+			
+		done
+		
+	done
+	
+	# Fix the IFS
+	IFS="$OFS"
+
+}
+
+function check_and_make_coassembly_dirs {
+	# Description: tests if coassembly directories exist (they should not!) and makes directories otherwise.
+	# GLOBAL params: OUTPUT_DIR, assembly_samples (array)
+	# Local params: none
+	# Return: mapping_names (array of assembly_names)
+	
+	for name in ${coassembly_names[@]}; do
+		coassembly_dir="${OUTPUT_DIR}/${name}"
+		
+		if [ ! -d ${coassembly_dir} ]; then
+			mkdir ${coassembly_dir}
+		else
+			echo "ERROR: found pre-existing output co-assembly directory ${coassembly_dir}. Please delete before starting. Job terminating."
+			exit 1
+		fi
+
+	done
+
 }
 
 function get_mapping_samples {
@@ -148,93 +254,52 @@ function concatenate_pre_assembly {
 
 }
 
-function normalize_coverage_across_kmers {
-	
-	mkdir -p ${CO_ASSEMBLY_DIR}/assembly/reads
-	mkdir -p ${CO_ASSEMBLY_DIR}/logs
-	
-	if [ in=${CO_ASSEMBLY_DIR}/sequence_quality_control/${CO_ASSEMBLY_NAME}_QC_se.fastq.gz != "null" ]; then
-		bbnorm.sh in=${CO_ASSEMBLY_DIR}/sequence_quality_control/${CO_ASSEMBLY_NAME}_QC_se.fastq.gz extra=${CO_ASSEMBLY_DIR}/sequence_quality_control/${CO_ASSEMBLY_NAME}_QC_R1.fastq.gz,${CO_ASSEMBLY_DIR}/sequence_quality_control/${CO_ASSEMBLY_NAME}_QC_R2.fastq.gz out=${CO_ASSEMBLY_DIR}/assembly/reads/normalized_se.fastq.gz k=21 t=100 interleaved=f minkmers=15 prefilter=t threads=14 -Xmx65G 2> ${CO_ASSEMBLY_DIR}/logs/${CO_ASSEMBLY_NAME}_normalization.log
-	fi
+# UNFINISHED
+function build_yaml {
+	# Description: Generates yaml file for user based on coassembly samples
 
+	atlas make-config --database-dir databases output/${CONFIG_FILEPATH} data
 
-
-# NOT DONE...
-	if [ t = "t" ]; then
-		bbnorm.sh in=NE1-sub100k/sequence_quality_control/NE1-sub100k_QC_R1.fastq.gz in2=NE1-sub100k/sequence_quality_control/NE1-sub100k_QC_R2.fastq.gz extra=NE1-sub100k/sequence_quality_control/NE1-sub100k_QC_se.fastq.gz out=NE1-sub100k/assembly/reads/normalized_R1.fastq.gz out2=NE1-sub100k/assembly/reads/normalized_R2.fastq.gz k=21 t=100 interleaved=f minkmers=15 prefilter=t threads=14 -Xmx32G 2>> NE1-sub100k/logs/NE1-sub100k_normalization.log
-	fi
 }
+
+# UNTESTED
+function run_atlas {
+	# Description: runs standard ATLAS pipeline, starting from assembly, for coassembly files
+	
+	local log_code=$(date '+%y%m%d_%H%M')
+	
+	# Define the step to force ATLAS to start from
+	local start_step=normalize_coverage_across_kmers
+	
+	# See what all steps of ATLAS would be without actually running ATLAS
+	atlas assemble --jobs ${threads} --out-dir output output/${CONFIG_FILEPATH} --force ${start_step} --dryrun > output/atlas_run_steps_${log_code}.log
+	
+	# Run ATLAS
+	atlas assemble --jobs ${threads} --out-dir output output/${CONFIG_FILEPATH} --force ${start_step} 2>&1 | tee output/atlas_run_${log_code}.log
+
+}
+
 
 function main {
 	echo "Running $(basename $0), version ${script_version}, on $(date)."
 	echo ""
 	
-	# Get inputs
 	test_inputs
-	get_assembly_samples # Now in variable 'assembly_names'
-	get_mapping_samples # Now in variable 'mapping_names'
-	echo "Running co-assembly for ${#assembly_names[@]} samples and mapping with ${#mapping_names[@]} samples."
-	echo ""
+	
+	# Get inputs
+	get_coassembly_params
+	
+	# Get date and time of start
+	start_time=$(date '+%y%m%d_%H%M')
+
+	
+	
+	#echo "Running co-assembly for ${#assembly_names[@]} samples and mapping with ${#mapping_names[@]} samples."
+	#echo ""
 	
 	concatenate_pre_assembly
 
 }
-
-
-# Test that the ATLAS conda binaries are where they should be
-
-
-# By default, set working directory to present working directory (pwd)
-work_dir=$(pwd)
-
-echo "Running $(basename $0), version $script_version."
-echo ""
-echo "Replacing items in file $(basename ${input_name}), based on the list provided in $(basename ${replacement_info})."
-
-
-cd "${work_dir}"
-
-# Temporarily change the internal fields separator (IFS) so that whitespaces in find/replace scheme do not create new entries. See Vince Buffalo's "Bioinformatics Data Skills" (1st Ed.) chapter 12, pg 407 and corresponding Github page README at https://github.com/vsbuffalo/bds-files/tree/master/chapter-12-pipelines (accessed Nov 19, 2017)
-OFS="$IFS"
-IFS=$'\n'
-
-# Get lists of old and new filenames and store as arrays
-names_old=($(tail -n +2 "${replacement_info}" | cut -d $'\t' -f 1))
-names_new=($(tail -n +2 "${replacement_info}" | cut -d $'\t' -f 2))
-# Got help from http://stackoverflow.com/questions/2961673/find-missing-argument-to-exec, post by Marian on June 2, 2010; accessed May 13, 2016
-
-# Fix the IFS
-IFS="$OFS"
-
-# Get number of names
-num_names=${#names_old[@]}
-
-# Confirm number of names is equal between lists
-if [ ${#names_old[@]} != ${#names_new[@]} ]
-then
-    echo "Error: ${#names_old[@]} entries in old names column and ${#names_new[@]} in new names column. These do not match. This could possibly be due to special characers in one of the columns, which this has limited support for. Exiting..."
-    exit 1
-else echo "Identified ${num_names} items for replacement."
-fi
-
-echo ""
-
-# Make a copy of the tree file for multiple in-place edits
-cp "${input_name}" "${output_name}"
-
-for i in $(seq 1 ${num_names})
-do
-# Get names
-    old_name="${names_old[i-1]}"
-    new_name="${names_new[i-1]}"
-    echo "${old_name} --> ${new_name}"
-    sed -i -e "s/${old_name}/${new_name}/g" $output_name
-done
-# "for" loop idea from Bioinformatics Data Skills (Vince Buffalo), Ch. 12
-# sed help from http://unix.stackexchange.com/a/159369 (main) and http://stackoverflow.com/a/15236526 (debugging), accessed Feb. 22, 2017
-
-# Remove backup file created by sed once done
-rm ${output_name}-e
 
 
 echo ""
