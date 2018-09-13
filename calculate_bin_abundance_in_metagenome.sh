22f27ea12d5f7eecf59ef26a448c2babe401e17e
Jackson M. Tsuji <jackson.tsuji@uwaterloo.ca>
2018-09-13 19:35:01 -0400

    Finished first draft

--- calculate_bin_abundance_in_metagenome.sh
+++ calculate_bin_abundance_in_metagenome.sh
@@ -1,67 +1,109 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-## To install needed dependendies:
-# conda create -y -n calc_bin_abundance -c conda-forge -c bioconda samtools bbmap
-# source calc_bin_abundance
+# If no input is provided, provide help and exit
+if [ $# == 0 ]
+    then
+    printf "$(basename $0): Wrapper to calculate the read recruitment of genome bins to metagenomes.\n"
+    printf "Version: ${script_version}\n"
+    printf "Contact Jackson M. Tsuji (jackson.tsuji@uwaterloo.ca) for bug reports or feature requests.\n\n"
+    printf "Installation: you can install all dependencies via conda and then add the scripts from the Github repo with:\n"
+    printf "              conda create -y -n calc_bin_abundance -c conda-forge -c bioconda samtools bbmap\n"
+    printf "              source activate calc_bin_abundance\n\n"
+    printf "Usage: $(basename $0) refined_bin_dir raw_read_dir output_dir threads memory 2>&1 | tee $(basename $0 .sh).log\n\n"
+    printf "Usage details:\n"
+    printf "   1. refined_bin_dir: Directory containing genomes/bins. MUST have extension *.fa!! Symlinks are okay.\n"
+    printf "   2. raw_read_dir: Directory containing QC'ed, unassembled metagenomic reads with naming structure matching that of the ATLAS pipeline.\n"
+    printf "                    *QC_R1.fastq.gz, *QC_R2.fastq.gz, and *QC_se.fastq.gz all needed. Symlinks are okay.\n"
+    printf "   3. output_dir: Directory where you want the results to be output. Anything already there might be overwritten.\n"
+    printf "   4. threads: number of threads to run.\n"
+    printf "   5. memory: in gigabytes.\n\n"
+    exit 1
+fi
 
-# Set user variables (hard-coded for now)
-# TODO - allow user to set themselves
-refined_bin_dir= # bins MUST have extension *.fa
-raw_read_dir= # reads MUST be output of ATLAS QC and match naming structure. R1, R2, se reads all need to be there, for all samples.
-output_dir=
-THREADS=40
-MEMORY=50
+# Set user variables
+refined_bin_dir=$1 # bins MUST have extension *.fa
+raw_read_dir=$2 # reads MUST be output of ATLAS QC and match naming structure. R1, R2, se reads all need to be there, for all samples.
+output_dir=$3
+THREADS=$4
+MEMORY=$5 # in Gigabytes
+
+(>&2 echo "[ $(date -u) ]: Running script with ${THREADS} threads and ${MEMORY} GB memory")
+(>&2 echo "[ $(date -u) ]: refined_bin_dir: ${refined_bin_dir}")
+(>&2 echo "[ $(date -u) ]: raw_read_dir: ${raw_read_dir}")
+(>&2 echo "[ $(date -u) ]: output_dir: ${output_dir}")
+(>&2 echo "[ $(date -u) ]: bin_mapping_summary_filename: bin_mapping_stats.tsv (in output_dir)")
 
 # Create folder structure in output dir
 mkdir -p ${output_dir}/mapping ${output_dir}/logs
-output_filename="${output_dir}/bin_mapping_stats.tsv"
+bin_mapping_summary_filename="${output_dir}/bin_mapping_stats.tsv"
 
 # Find bin and raw read files
 bin_paths=($(find -L ${refined_bin_dir} -iname "*.fa")) # Careful - make sure the files match *.fa!
 raw_read_paths=($(find -L ${raw_read_dir} -iname "*R1.fastq.gz")) # Grab the R1 only
+iterations=$((${#bin_paths[@]}*${#raw_read_paths[@]}))
+
+(>&2 echo "[ $(date -u) ]: found ${#bin_paths[@]} genome bins with extension *.fa in the refined_bin_dir")
+(>&2 echo "[ $(date -u) ]: found ${#raw_read_paths[@]} set of unassembled metagenomic reads in the raw_read_dir")
+(>&2 echo "[ $(date -u) ]: this means found ${#bin_paths[@]} * ${#raw_read_paths[@]} = ${iterations} combinations of mapping will be run")
 
 # Initialize output read stats file
-# TODO - add column for average coverage
-printf 'mapping_filename\tgenome\tmetagenome\tmapped_reads\ttotal_reads\n' > ${output_filename}
+(>&2 echo "[ $(date -u) ]: Initiatilizing '${bin_mapping_summary_filename##*/}'")
+printf "mapping_filename\tgenome\tmetagenome\tmapped_reads\ttotal_reads\tgenome_length_nt\t" > ${bin_mapping_summary_filename}
+printf "percent_recruited_reads\taverage_coverage\taverage_coverage_per_million_reads\n" >> ${bin_mapping_summary_filename}
 
-# Run all against all in a nested for loop
+# Run all against all in a nested 'for' loop
 for bin_path in ${bin_paths}; do
-for raw_read_path in ${raw_read_paths}; do
 
-# Get base names of bins and reads
-bin_name_base=${bin_path%.*}
-bin_name_base=${bin_name_base##*/}
-raw_read_name_base=${raw_read_path%*R1.fastq.gz}
-raw_read_name_base=${raw_read_name_base##*/}
+	# Get total nucleotide length in bin for later
+	genome_char_count=$(grep -v "^>" ${bin_path} | wc)
+	genome_new_lines=$(echo ${genome_char_count} | cut -d ' ' -f 1)
+	genome_total_chars=$(echo ${genome_char_count} | cut -d ' ' -f 3)
+	genome_length_nt=$((${genome_total_chars}-${genome_new_lines}))
 
-# Assign generic variables for read mapping
-R1=${raw_read_dir}/${raw_read_name_base}R1.fastq.gz
-R2=${raw_read_dir}/${raw_read_name_base}R2.fastq.gz
-se=${raw_read_dir}/${raw_read_name_base}se.fastq.gz
-contigs=${bin_path}
-outfile=${output_dir}/mapping/${bin_name_base}_to_${raw_read_name_base}.sam
-logfile=${output_dir}/logs/${bin_name_base}_to_${raw_read_name_base}_contig_coverage_stats.log
+	(>&2 echo "[ $(date -u) ]: Starting on bin '${bin_path##*/}' with length ${genome_length_nt} nt")
 
-# Read map
-(>&2 echo "[$(date '+%y%m%d %H:%M:%S %Z')]: ${sample_name}: mapping '${raw_read_name_base}*.fastq.gz' to '${bin_name_base}'.")
-bbwrap.sh nodisk=t ref=${contigs} in1=${R1},${se} in2=${R2},null perfectmode=t trimreaddescriptions=t \
-	out=${outfile} threads=${THREADS} pairlen=1000 pairedonly=t mdtag=t xstag=fs nmtag=t sam=1.3 \
-	local=t ambiguous=best secondary=t ssao=t maxsites=10 -Xmx${MEMORY}G 2> ${logfile}
+	for raw_read_path in ${raw_read_paths}; do
 
-(>&2 echo "[ $(date -u) ]: Calculating stats for ${mapping_filename##*/}")
+		# Get base names of bins and reads
+		bin_name_base=${bin_path%.*}
+		bin_name_base=${bin_name_base##*/}
+		raw_read_name_base=${raw_read_path%*R1.fastq.gz}
+		raw_read_name_base=${raw_read_name_base##*/}
 
-# Calculate mapping stats
-# TODO - add average coverage
-mapped_reads=$(samtools view -@ ${THREADS} -c -F 4 ${outfile})
-total_reads=$(samtools view -@ ${THREADS} -c ${outfile})
-(>&2 echo "[ $(date -u) ]: ${outfile##*/}: ${mapped_reads} mapped; ${total_reads} total.")
+		# Assign generic variables for read mapping
+		R1=${raw_read_dir}/${raw_read_name_base}R1.fastq.gz
+		R2=${raw_read_dir}/${raw_read_name_base}R2.fastq.gz
+		se=${raw_read_dir}/${raw_read_name_base}se.fastq.gz
+		contigs=${bin_path}
+		read_mapping_file=${output_dir}/mapping/${bin_name_base}_to_${raw_read_name_base}.sam
+		logfile=${output_dir}/logs/${bin_name_base}_to_${raw_read_name_base}_contig_coverage_stats.log
 
-# Add to TSV file
-# TODO - add average coverage
-printf "${outfile##*/}\t${bin_name_base}\t${raw_read_name_base}\t${mapped_reads}\t${total_reads}\n" >> ${output_filename}
+		# Read map
+		(>&2 echo "[$(date '+%y%m%d %H:%M:%S %Z')]: ${sample_name}: mapping '${raw_read_name_base}*.fastq.gz' to '${bin_name_base}'")
+		bbwrap.sh nodisk=t ref=${contigs} in1=${R1},${se} in2=${R2},null perfectmode=t trimreaddescriptions=t \
+			out=${read_mapping_file} threads=${THREADS} pairlen=1000 pairedonly=t mdtag=t xstag=fs nmtag=t sam=1.3 \
+			local=t ambiguous=best secondary=t ssao=t maxsites=10 -Xmx${MEMORY}G 2> ${logfile}
 
-done
+		(>&2 echo "[ $(date -u) ]: Calculating stats for '${mapping_filename##*/}'")
+
+		# Extract mapping stats
+		mapped_reads=$(samtools view -@ ${THREADS} -c -F 4 ${read_mapping_file})
+		total_reads=$(samtools view -@ ${THREADS} -c ${read_mapping_file})
+		(>&2 echo "[ $(date -u) ]: ${read_mapping_file##*/}: ${mapped_reads} mapped; ${total_reads} total.")
+
+		# Calculate additional stats
+		percent_recruited_reads=$((${mapped_reads}/${total_reads}*100))
+		average_coverage=$((${mapped_reads}/${genome_length_nt}))
+		average_coverage_per_million_reads=$((${average_coverage}/${total_reads}*1000000))
+
+		# Add to TSV file
+		printf "${read_mapping_file##*/}\t${bin_name_base}\t${raw_read_name_base}\t${mapped_reads}\t${total_reads}\t${genome_length_nt}\t" >> ${bin_mapping_summary_filename}
+		printf "${percent_recruited_reads}\t${average_coverage}\t${average_coverage_per_million_reads}\n" >> ${bin_mapping_summary_filename}
+
+
+	done
 done
 
 (>&2 echo "[ $(date -u) ]: Finished.")
+
